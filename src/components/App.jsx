b71fbc3b6430af1c38ca81b9488cd45cb47df099
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-20 05:38:46 +0300

    refactored with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { mapper } from 'utils/mapper';
@@ -9,90 +9,126 @@ import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Button } from './Button/Button';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    currentImage: null,
-    page: 1,
-    loading: false,
-    total: 0,
-    perPage: 12,
-  };
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     images: [],
+//     currentImage: null,
+//     page: 1,
+//     loading: false,
+//     total: 0,
+//     perPage: 12,
+//   };
 
-  componentDidUpdate(_, prevState) {
-    const {
-      state: { query, page },
-      fetchImages,
-    } = this;
-    if (prevState.query !== query || prevState.page !== page) {
-      fetchImages();
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [currentImage, setCurrentImage] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [total, setTotal] = useState(0);
+  const [perPage, setPerPage] = useState(12);
+
+  // componentDidUpdate(_, prevState) {
+  //   const {
+  //     state: { query, page },
+  //     fetchImages,
+  //   } = this;
+  //   if (prevState.query !== query || prevState.page !== page) {
+  //     fetchImages();
+  //   }
+  // }
+
+  // const fetchImages = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const imagesData = await fetchImagesWithQuery(query, page);
+
+  //     setImages([...images, ...mapper(imagesData.data.hits)]);
+  //     setTotal(imagesData.data.total);
+  //   } catch (error) {
+  //     toast.error(`${error}`);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
+    setLoading(true);
+    fetchImagesWithQuery(query, page)
+      .then(imagesData => {
+        setImages(prevImages => [
+          ...prevImages,
+          ...mapper(imagesData.data.hits),
+        ]);
+        setTotal(imagesData.data.total);
+      })
+      .catch(error => {
+        toast.error(`${error}`);
+      })
+      .finally(() => setLoading(false));
+  }, [query, page]);
 
-  handleFormSubmit = query => {
-    this.setState({
-      query,
-      page: 1,
-      images: [],
-    });
+  const handleFormSubmit = query => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
   };
 
-  updateCurrentImage = data => {
-    this.setState({ currentImage: data });
+  const openModal = data => {
+    setCurrentImage(data);
   };
 
-  closeModal = () => {
-    this.setState({ currentImage: null });
+  const closeModal = () => {
+    setCurrentImage(null);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  fetchImages = async () => {
-    try {
-      const { query, page } = this.state;
-      this.setState({ loading: true });
-      const imagesData = await fetchImagesWithQuery(query, page);
+  // const fetchImages = async () => {
+  //   try {
 
-      this.setState(prevState => ({
-        images: [...prevState.images, ...mapper(imagesData.data.hits)],
-        total: imagesData.data.total,
-      }));
-    } catch (error) {
-      toast.error(`${error}`);
-    } finally {
-      this.setState({ loading: false });
-    }
-  };
+  //     setLoading(true);
+  //     const imagesData = await fetchImagesWithQuery(query, page);
 
-  render() {
-    const {
-      state: { loading, query, images, currentImage, total, page, perPage },
-      handleFormSubmit,
-      updateCurrentImage,
-      closeModal,
-      loadMore,
-    } = this;
-    return (
-      <>
-        <SearchBar onSubmit={handleFormSubmit} />
-        {loading && <Loader />}
-        {query && (
-          <ImageGallery openModal={updateCurrentImage} images={images} />
-        )}
-        {currentImage && <Modal src={currentImage} closeModal={closeModal} />}
+  //     this.setState(prevState => ({
+  //       images: [...prevState.images, ...mapper(imagesData.data.hits)],
+  //       total: imagesData.data.total,
+  //     }));
+  //   } catch (error) {
+  //     toast.error(`${error}`);
+  //   } finally {
+  //     this.setState({ loading: false });
+  //   }
+  // };
 
-        {Math.ceil(total / page) > perPage && (
-          <Button handlerLoadMore={loadMore} />
-        )}
-        <ToastContainer
-          autoClose={3000}
-          position="top-center"
-          hideProgressBar={false}
-        />
-      </>
-    );
-  }
-}
+  // const {
+  //   state: { loading, query, images, currentImage, total, page, perPage },
+  //   handleFormSubmit,
+  //   updateCurrentImage,
+  //   closeModal,
+  //   loadMore,
+  // } = this;
+  return (
+    <>
+      <SearchBar onSubmit={handleFormSubmit} />
+      {loading && <Loader />}
+      {query && <ImageGallery openModal={openModal} images={images} />}
+      {currentImage && <Modal src={currentImage} closeModal={closeModal} />}
+
+      {Math.ceil(total / page) > perPage && (
+        <Button handlerLoadMore={loadMore} />
+      )}
+      <ToastContainer
+        autoClose={3000}
+        position="top-center"
+        hideProgressBar={false}
+      />
+    </>
+  );
+};
