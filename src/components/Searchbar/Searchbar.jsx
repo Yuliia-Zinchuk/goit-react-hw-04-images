29a6baf1bc0769cf2b0d23f4687c389b926a4e0c
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-20 03:11:21 +0300

    refactored with hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,55 +1,68 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
-
 import css from './Searchbar.module.css';
 
-export class SearchBar extends Component {
-  state = {
-    imageQuery: '',
+// export class SearchBar extends Component {
+//   state = {
+//     imageQuery: '',
+//   };
+export const SearchBar = ({ onSubmit }) => {
+  const [imageQuery, setImageQuery] = useState('');
+
+  // handleImageChange = event => {
+  //   this.setState({ imageQuery: event.currentTarget.value.toLowerCase() });
+  // };
+
+  const handleImageChange = event => {
+    setImageQuery(event.currentTarget.value.toLowerCase());
   };
 
-  handleImageChange = event => {
-    this.setState({ imageQuery: event.currentTarget.value.toLowerCase() });
-  };
+  // handleSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.imageQuery.trim() === '') {
+  //     return toast.warning('Please, enter your request!');
+  //   }
+  //   this.props.onSubmit(this.state.imageQuery);
 
-  handleSubmit = event => {
+  //   this.setState({ imageQuery: '' });
+  // };
+
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.imageQuery.trim() === '') {
+    if (imageQuery.trim() === '') {
       return toast.warning('Please, enter your request!');
     }
-    this.props.onSubmit(this.state.imageQuery);
+    onSubmit(imageQuery);
 
-    this.setState({ imageQuery: '' });
+    setImageQuery('');
   };
 
-  render() {
-    const {
-      state: { imageQuery },
-      handleImageChange,
-    } = this;
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.SearchForm_button}>
-            <span className={css.SearchForm_button_label}>Search</span>
-          </button>
+  // const {
+  //   state: { imageQuery },
+  //   handleImageChange,
+  // } = this;
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchForm_button}>
+          <span className={css.SearchForm_button_label}>Search</span>
+        </button>
 
-          <input
-            className={css.SearchForm_input}
-            type="text"
-            name="imageQuery"
-            value={imageQuery}
-            onChange={handleImageChange}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.SearchForm_input}
+          type="text"
+          name="imageQuery"
+          value={imageQuery}
+          onChange={handleImageChange}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 SearchBar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
